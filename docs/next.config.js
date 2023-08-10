fef3fcd895c753a8eb6420cd029e9da0744e590c
vaynevayne <740652572@qq.com>
2023-08-10 11:58:44 +0800

    build(docs): add transpile

---
+++ docs/next.config.js
@@ -0,0 +1,17 @@
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.jsx",
+});
+
+module.exports = withNextra({
+  transpilePackages: ["@get-ui/react", "@get-ui/theme"],
+  swcMinify: true,
+  reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
+  // redirects: require("./next-redirect.js"),
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+});
+
+// If you have other Next.js configurations, you can pass them as the parameter:
+// module.exports = withNextra({ /* other next.js config */ })
